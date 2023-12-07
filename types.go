6ca13eaff861ae11722ad6fce715646280957dce
Victor Agudo <victoragudo88@gmail.com>
2023-12-07 16:18:05 +0100

    Refactored ICommandHandler, IQueryHandler, and IEventHandler interfaces to use generic type parameters TCommand, TResponse, TQuery, TEvent respectively, and updated the method signatures accordingly.

--- types.go
+++ types.go
@@ -12,17 +12,17 @@ type (
 	}
 
 	// ICommandHandler defines the interface for a command handler with a request and response of generic type.
-	ICommandHandler[CommandRequest T, CommandResponse T] interface {
-		Handle(ctx context.Context, commandRequest CommandRequest) (commandResponse CommandResponse, err error)
+	ICommandHandler[TCommand T, TResponse T] interface {
+		Handle(ctx context.Context, command TCommand) (response TResponse, err error)
 	}
 
 	// IQueryHandler defines the interface for a query handler with a request and response of generic type.
-	IQueryHandler[QueryRequest T, QueryResponse T] interface {
-		Handle(ctx context.Context, queryRequest QueryRequest) (queryResponse QueryResponse, err error)
+	IQueryHandler[TQuery T, TResponse T] interface {
+		Handle(ctx context.Context, query TQuery) (response TResponse, err error)
 	}
 
 	// IEventHandler defines the interface for an event handler with an event of generic type.
-	IEventHandler[Event T] interface {
-		Handle(ctx context.Context, event Event) (err error)
+	IEventHandler[TEvent T] interface {
+		Handle(ctx context.Context, event TEvent) (err error)
 	}
 )
