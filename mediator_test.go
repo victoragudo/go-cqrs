52b6eedfa283c8a952711969006b69b10e9e8002
Víctor Agudo <victoragudo88@gmail.com>
2023-12-08 15:26:11 +0100

    Implementation of new unit tests (#1)
    * Add new test for sending unregistered command and checking response.
    
    * Refactor mediator_test.go to add test cases for command and event handling, and test concurrency.

--- mediator_test.go
+++ mediator_test.go
@@ -2,76 +2,232 @@ package gocqrs
 
 import (
 	"context"
-	"errors"
+	"reflect"
 	"testing"
 )
 
-type commandHandlerTest struct {
-	callbackFunc func(cmdRequestTest commandRequestTest) (commandResponseTest, error)
-}
-type commandRequestTest struct {
-	data string
-}
-type commandResponseTest struct {
-	data string
+// MockCommandHandler implements ICommandHandler for testing purposes.
+type MockCommandHandler struct{}
+
+func (m *MockCommandHandler) Handle(ctx context.Context, command string) (string, error) {
+	return "handled: " + command, nil
 }
 
-func (cmdHandlerTest *commandHandlerTest) Handle(ctx context.Context, cmdRequestTest commandRequestTest) (commandResponseTest, error) {
-	return cmdHandlerTest.callbackFunc(cmdRequestTest)
-}
-
-func newCommandHandlerWrapperTest(f func(cmdRequestTest commandRequestTest) (commandResponseTest, error)) *commandHandlerTest {
-	return &commandHandlerTest{
-		callbackFunc: f,
+// Helper functions for assertions
+func assertNilError(t *testing.T, err error) {
+	if err != nil {
+		t.Errorf("Expected no error, but got %v", err)
 	}
 }
 
+func assertNotNilError(t *testing.T, err error) {
+	if err == nil {
+		t.Errorf("Expected an error, but got none")
+	}
+}
+
+func assertEqual(t *testing.T, expected, actual interface{}) {
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}
+
+// TestCommandHandlerWrapper_Handle tests the Handle method of commandHandlerWrapper.
+func TestCommandHandlerWrapper_Handle(t *testing.T) {
+	ctx := context.Background()
+	command := "test command"
+	mockHandler := &MockCommandHandler{}
+	wrapper := newCommandHandlerWrapper[string, string](mockHandler)
+
+	// Success case
+	got, err := wrapper.Handle(ctx, command)
+	assertNilError(t, err)
+	assertEqual(t, "handled: "+command, got)
+
+	// Error case: incorrect type
+	_, err = wrapper.Handle(ctx, 123) // 123 is int, not string
+	assertNotNilError(t, err)
+}
+
+// TestAddCommandHandler tests the AddCommandHandler function.
 func TestAddCommandHandler(t *testing.T) {
+	mockHandler := &MockCommandHandler{}
+	AddCommandHandler[string, string](mockHandler)
 
-	type args[CommandRequest T, CommandResponse T] struct {
-		handler        ICommandHandler[CommandRequest, CommandResponse]
-		context        context.Context
-		commandRequest commandRequestTest
+	// Verify if the handler was added correctly
+	handler, ok := commandHandlers.Load("string")
+	if !ok {
+		t.Fatal("Handler not found in commandHandlers")
 	}
-	type expected struct {
-		err error
-	}
-	type testCase[CommandRequest T, CommandResponse T] struct {
-		name     string
-		args     args[CommandRequest, CommandResponse]
-		expected expected
-	}
-	tests := []testCase[commandRequestTest, commandResponseTest]{
-		{
-			name: "CommandHandler_SendCommand",
-			args: args[commandRequestTest, commandResponseTest]{
-				handler: newCommandHandlerWrapperTest(func(cmdRequestTest commandRequestTest) (commandResponseTest, error) {
-					return commandResponseTest{
-						data: cmdRequestTest.data,
-					}, nil
-				}),
-				context: context.Background(),
-				commandRequest: commandRequestTest{
-					data: "data",
-				},
-			},
-			expected: expected{
-				err: nil,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			AddCommandHandler(tt.args.handler)
-			commandResponse, err := SendCommand[commandResponseTest](tt.args.context, tt.args.commandRequest)
 
-			if commandResponse.data != tt.args.commandRequest.data {
-				t.Errorf("data are not equal")
+	// Assert that it's the correct type
+	_, ok = handler.(*commandHandlerWrapper[string, string])
+	if !ok {
+		t.Errorf("Handler is not of type *commandHandlerWrapper[string, string]")
+	}
+}
+
+// TestSendCommand tests the SendCommand function.
+func TestSendCommand(t *testing.T) {
+	ctx := context.Background()
+	command := "test command"
+	AddCommandHandler[string, string](&MockCommandHandler{})
+
+	// Success case
+	response, err := SendCommand[string](ctx, command)
+	assertNilError(t, err)
+	assertEqual(t, "handled: "+command, response)
+
+	// Error case: no registered handler
+	_, err = SendCommand[int](ctx, 123)
+	assertNotNilError(t, err)
+}
+
+// MockEventHandler for events
+type MockEventHandler struct{}
+
+func newMockEventHandler() *MockEventHandler {
+	return &MockEventHandler{}
+}
+
+func (m *MockEventHandler) Handle(ctx context.Context, event string) error {
+	return nil
+}
+
+// TestPublishEvent tests the PublishEvent function.
+func TestPublishEvent(t *testing.T) {
+	ctx := context.Background()
+	event := "test event"
+	AddEventHandlers[string](newMockEventHandler())
+
+	// Success case
+	err := PublishEvent(ctx, event)
+	assertNilError(t, err)
+
+	// Error case: no registered handlers
+	err = PublishEvent(ctx, 123) // 123 is int, a different type
+	assertNotNilError(t, err)
+}
+
+// TestSendCommand_Concurrency tests the SendCommand function for concurrent access.
+func TestSendCommand_Concurrency(t *testing.T) {
+	ctx := context.Background()
+	command := "test command"
+	AddCommandHandler[string, string](&MockCommandHandler{})
+
+	// Number of goroutines to simulate concurrency.
+	numGoroutines := 100
+
+	// Channel to receive responses and errors from each goroutine.
+	responses := make(chan string, numGoroutines)
+	errors := make(chan error, numGoroutines)
+
+	// Execute SendCommand in multiple goroutines concurrently.
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			response, err := SendCommand[string](ctx, command)
+			if err != nil {
+				errors <- err
+				return
 			}
+			responses <- response
+		}()
+	}
 
-			if !errors.Is(err, tt.expected.err) {
-				t.Errorf("returned error is not equal")
+	// Check the responses and errors.
+	for i := 0; i < numGoroutines; i++ {
+		select {
+		case response := <-responses:
+			expectedResponse := "handled: " + command
+			if response != expectedResponse {
+				t.Errorf("Expected response %v, got %v", expectedResponse, response)
 			}
-		})
+		case err := <-errors:
+			t.Errorf("Received error: %v", err)
+		}
+	}
+}
+
+// TestSendQuery_Concurrency tests the SendQuery function for concurrent access.
+func TestSendQuery_Concurrency(t *testing.T) {
+	ctx := context.Background()
+	query := "test query"
+	AddQueryHandler[string, string](&MockQueryHandler{})
+
+	// Number of goroutines to simulate concurrency.
+	numGoroutines := 100
+
+	// Channel to receive responses and errors from each goroutine.
+	responses := make(chan string, numGoroutines)
+	errors := make(chan error, numGoroutines)
+
+	// Execute SendQuery in multiple goroutines concurrently.
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			response, err := SendQuery[string](ctx, query)
+			if err != nil {
+				errors <- err
+				return
+			}
+			responses <- response
+		}()
+	}
+
+	// Check the responses and errors.
+	for i := 0; i < numGoroutines; i++ {
+		select {
+		case response := <-responses:
+			expectedResponse := "handled: " + query
+			if response != expectedResponse {
+				t.Errorf("Expected response %v, got %v", expectedResponse, response)
+			}
+		case err := <-errors:
+			t.Errorf("Received error: %v", err)
+		}
+	}
+}
+
+// MockQueryHandler implements IQueryHandler for testing purposes.
+type MockQueryHandler struct{}
+
+func (m *MockQueryHandler) Handle(ctx context.Context, query string) (string, error) {
+	return "handled: " + query, nil
+}
+
+// TestPublishEvent_Concurrency tests the PublishEvent function with multiple handlers under concurrent conditions.
+func TestPublishEvent_Concurrency(t *testing.T) {
+	ctx := context.Background()
+	event := "test event"
+
+	// Number of event handlers and goroutines to simulate concurrency.
+	numHandlers := 10
+	numGoroutines := 100
+
+	// Create and register multiple event handlers
+	for i := 0; i < numHandlers; i++ {
+		AddEventHandlers[string](newMockEventHandler())
+	}
+
+	// Channel to receive errors from each goroutine.
+	errors := make(chan error, numGoroutines)
+
+	// Execute PublishEvent in multiple goroutines concurrently.
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			err := PublishEvent(ctx, event)
+			if err != nil {
+				errors <- err
+			} else {
+				errors <- nil
+			}
+		}()
+	}
+
+	// Check for any errors received.
+	for i := 0; i < numGoroutines; i++ {
+		err := <-errors
+		if err != nil {
+			t.Errorf("Received error: %v", err)
+		}
 	}
 }
